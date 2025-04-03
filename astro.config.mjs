c9ff776738a0ba93bbc7394bc9a27d0a17388e2a
Stirling Hostetter <stirlhoss@proton.me>
2025-04-03 16:04:42 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  i18n: {
+    prefixDefaultLocale: false,
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+
+  integrations: [
+    starlight({
+      title: "Docs",
+      logo: { src: "./src/images/BABLRTransparent.png" },
+      prerender: false,
+      favicon: "/favicon.ico",
+      social: {
+        github: "https://github.com/bablr-lang/bablr-docs",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+  server: { port: 4321 },
+  site: "https://build.bablr.org",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
