c617d5b95697e68a2059b6584d860205b6ffeb9a
Conrad Buck <conartist6@gmail.com>
2026-01-29 14:54:18 -0700

    Fix and bump deps

--- src/content/docs/components/Highlight.js
+++ src/content/docs/components/Highlight.js
@@ -1,18 +1,22 @@
 import { highlightAll } from 'bedazzlr';
+import * as Spans from '@bablr/agast-helpers/spans';
+import { buildSpan } from '@bablr/agast-helpers/builders';
 import cstml from '@bablr/language-en-cstml';
 import esnext from '@bablr/language-en-esnext';
 import json from '@bablr/language-en-json';
 
 let languages = new Map([
-	[cstml.canonicalURL, cstml],
-	[esnext.canonicalURL, esnext],
-	[json.canonicalURL, json],
+  [cstml.canonicalURL, cstml],
+  [esnext.canonicalURL, esnext],
+  [json.canonicalURL, json],
 ]);
 
 const Highlighter = () => {
-	return null;
+  return null;
 };
 
-highlightAll(languages);
+highlightAll(languages, {
+  spans: Spans.fromValues([buildSpan('Trivia', null, { spaces: 2 })]),
+});
 
 export default Highlighter;
